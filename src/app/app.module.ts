7f0016bfb0e1e4f388cf8bbb4668c888a2e780af
Neo <neolwc@gmail.com>
2016-11-15 17:51:04 -0500

    Add the ContactComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,26 @@ import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { TitleComponent } from './title/title.component';
 import { UserService } from './user.service';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './contact/contact.service';
+import { AwesomePipe } from './contact/awesome.pipe';
+import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HighlightDirective,
-    TitleComponent
+    TitleComponent,
+    ContactComponent,
+    AwesomePipe,
+    ContactHighlightDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [ UserService ],
+  providers: [ ContactService, UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
