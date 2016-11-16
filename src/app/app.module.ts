95f80a5afe48da7ff8dcf46ba4560d8dc0e75754
Neo <neolwc@gmail.com>
2016-11-16 10:49:00 -0500

    Make Contact a feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { TitleComponent } from './title/title.component';
 import { UserService } from './user.service';
-import { ContactComponent } from './contact/contact.component';
-import { ContactService } from './contact/contact.service';
-import { AwesomePipe } from './contact/awesome.pipe';
-import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive';
+
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HighlightDirective,
-    TitleComponent,
-    ContactComponent,
-    AwesomePipe,
-    ContactHighlightDirective
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [ ContactService, UserService ],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, ContactModule ],
+  declarations: [ AppComponent, HighlightDirective, TitleComponent ],
+  providers:    [ UserService ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
